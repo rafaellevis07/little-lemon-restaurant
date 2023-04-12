91423d0f55e0d35a1442f93cb780514f45a5552b
Rafael Garcia <rafaellevis07@gmail.com>
2023-04-12 16:42:36 -0700

    set: Initial components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import Header from "Header.js";
+import Nav from "Nav.js";
+import Main from "Main.js";
+import Footer from "Footer.js";
 
-function App() {
-  return <div className="App">Homepage</div>;
-}
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
+    </>
+  );
+};
 
 export default App;
