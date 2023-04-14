4b5e70487a75a553b3afc31d014561255cf7a6a1
Rafael Garcia <rafaellevis07@gmail.com>
2023-04-13 17:16:12 -0700

    footer information

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,6 +7,43 @@ const Footer = () => {
         <img src={footerLogo} alt="Little Lemon Footer Logo" />
       </li>
       <h1>Navigation</h1>
+      <ul>
+        {" "}
+        <li>
+          <a href="/">HOME</a>
+        </li>
+        <li>
+          <a href="/about">ABOUT</a>
+        </li>
+        <li>
+          <a href="/menu">MENU</a>
+        </li>
+        <li>
+          <a href="/reservations">RESERVATIONS</a>
+        </li>
+        <li>
+          <a href="/order">ORDER ONLINE</a>
+        </li>
+        <li>
+          <a href="/login">LOGIN</a>
+        </li>
+      </ul>
+      <h1>Contact</h1>
+      <h2>2395 Maldova Way, Chicago IL</h2>
+      <h2>(629)243-6827</h2>
+      <h2>info@littlelemon.com</h2>
+      <h1>Connect</h1>
+      <ul>
+        <li>
+          <a href="facebook.com">Facebook</a>
+        </li>
+        <li>
+          <a href="instagram.com">Instagram</a>
+        </li>
+        <li>
+          <h2>Join us</h2>
+        </li>
+      </ul>
     </footer>
   );
 };
