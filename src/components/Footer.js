da245f7f5668697df1d924a6de32481f11d6ac8e
Rafael Garcia <rafaellevis07@gmail.com>
2023-04-13 18:41:49 -0700

    Fix: Footer HTML

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,49 +1,60 @@
 import footerLogo from "../assets/footer-logo.png";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <footer>
-      <li>
-        <img src={footerLogo} alt="Little Lemon Footer Logo" />
-      </li>
-      <h1>Navigation</h1>
-      <ul>
-        {" "}
-        <li>
-          <a href="/">HOME</a>
+      <menu>
+        <li className="img-footer">
+          <img src={footerLogo} alt="Little Lemon Footer Logo" />
+        </li>
+        <li className="contact">
+          <h1 className="footer-header">Navigation</h1>
+          <ul className="footer-links">
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/menu">Menu</Link>
+            <Link to="/reservations">Reservation</Link>
+            <Link to="/orders">Order Online</Link>
+            <Link to="/login">Login</Link>
+          </ul>
         </li>
         <li>
-          <a href="/about">ABOUT</a>
+          <h1 className="footer-header">Contact</h1>
+          <ul className="footer-links">
+            <li>2395 Maldove Way,</li>
+            <li>Chicago, IL</li>
+            <li>(629)-243-6827</li>
+            <li>info@littlelemon.com</li>
+          </ul>
         </li>
         <li>
-          <a href="/menu">MENU</a>
-        </li>
-        <li>
-          <a href="/reservations">RESERVATIONS</a>
-        </li>
-        <li>
-          <a href="/order">ORDER ONLINE</a>
-        </li>
-        <li>
-          <a href="/login">LOGIN</a>
-        </li>
-      </ul>
-      <h1>Contact</h1>
-      <h2>2395 Maldova Way, Chicago IL</h2>
-      <h2>(629)243-6827</h2>
-      <h2>info@littlelemon.com</h2>
-      <h1>Connect</h1>
-      <ul>
-        <li>
-          <a href="facebook.com">Facebook</a>
-        </li>
-        <li>
-          <a href="instagram.com">Instagram</a>
-        </li>
-        <li>
-          <h2>Join us</h2>
-        </li>
-      </ul>
+          <h1 className="footer-header">Connect</h1>
+          <ul className="footer-links">
+            <a
+              href="https://www.facebook.com/thelittlelemonshop/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Facebook
+            </a>
+            <a
+              href="https://www.instagram.com/littlelemonmoon/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Instagram
+            </a>
+            <a
+              href="https://www.youtube.com/watch?v=3Li-FfypZYE"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Join us!
+            </a>
+          </ul>
+        </li>{" "}
+      </menu>
     </footer>
   );
 };
