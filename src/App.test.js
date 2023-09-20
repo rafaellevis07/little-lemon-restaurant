de0625ccf3995b6456921047e499acf3d9136897
Rafael Garcia <rafaellevis07@gmail.com>
2023-09-20 10:50:59 -0700

    deletion of test

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import BookingForm from "../src/components/pages/BookingPage";
-
-test("Renders the BookingForm heading", () => {
-  render(<BookingForm />);
-  const headingElement = screen.getByText("Additional Comments");
-  expect(headingElement).toBeInTheDocument();
-});
